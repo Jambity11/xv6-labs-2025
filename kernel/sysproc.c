73b3b783ff7074e9e381777e80fe0ec4908cc43c
JambitX11 <JambitX2006@outlook.com>
2026-07-14 14:40:33 +0800

    Lab syscall: Sandbox a command

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -105,3 +105,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_interpose(void)
+{
+  int mask;
+  struct proc *p = myproc();
+
+  argint(0, &mask);
+  p->syscall_mask = (uint)mask;
+
+  return 0;
+}
