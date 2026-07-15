125b30a83b47800330b7414c78723701140f2241
JambitX11 <JambitX2006@outlook.com>
2026-07-15 14:26:01 +0800

    pgtbl: print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -142,9 +142,35 @@ walkaddr(pagetable_t pagetable, uint64 va)
 
 
 #if defined(LAB_PGTBL) || defined(SOL_MMAP) || defined(SOL_COW)
+
+static void
+vmprintwalk(pagetable_t pagetable, int level, uint64 va)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if((pte & PTE_V) == 0)
+      continue;
+
+    uint64 childva = va | ((uint64)i << PXSHIFT(level));
+    uint64 pa = PTE2PA(pte);
+
+    for(int j = 0; j < 3 - level; j++)
+      printf(" ..");
+
+    printf("%p: pte %p pa %p\n", (void*)childva, (void*)pte, (void*)pa);
+
+    if(level > 0 && (pte & (PTE_R | PTE_W | PTE_X)) == 0){
+      vmprintwalk((pagetable_t)pa, level - 1, childva);
+    }
+  }
+}
+
 void
-vmprint(pagetable_t pagetable) {
-  // your code here
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintwalk(pagetable, 2, 0);
 }
 #endif
 
